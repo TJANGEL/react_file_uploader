fcfc2d6ff0433cd4d6864a6d45c2df8e9259ec58
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-04-11 11:11:37 -0400

    added Message component to FileUpload Component

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import axios from "axios";
+import Message from "./Message";
 
 const FileUpload = () => {
   const [file, setFile] = useState("");
@@ -40,6 +41,7 @@ const FileUpload = () => {
 
   return (
     <Fragment>
+      {message ? <Message msg={message} /> : null}
       <form onSubmit={onSubmit}>
         <div className="custom-file mb-4">
           <input
